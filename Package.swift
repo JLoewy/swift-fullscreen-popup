1da81e594f31c2b3fa9072c58f20d35bb93e4f2b
Jason Loewy <jasonloewy@gmail.com>
2025-08-05 06:57:30 -0400

    Update min platform iOS

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "swift-fullscreen-popup",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v17),
         .macOS(.v14),
         .visionOS(.v1)
     ],
